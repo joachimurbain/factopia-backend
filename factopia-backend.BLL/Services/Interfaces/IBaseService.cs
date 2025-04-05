1c288d997cb437434b95b95ea35694ae00bd501a
Pierre Bossis <pbossis@hotmail.com>
2025-04-05 18:39:35 +0200

    Service generique

---
+++ factopia-backend.BLL/Services/Interfaces/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿using factopia_backend.Domain.Interfaces;
+
+namespace factopia_backend.BLL.Services.Interfaces;
+
+public interface IBaseService<T> where T : class, IEntity
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> AddAsync(T entity);
+    Task RemoveAsync(int id);
+}
--- factopia-backend.DAL/Repositories/Interfaces/IBaseRepository.cs
+++ factopia-backend.DAL/Repositories/Interfaces/IBaseRepository.cs
@@ -4,8 +4,8 @@ namespace factopia_backend.DAL.Repositories.Interfaces;
 
 public interface IBaseRepository<T> where T : class, IEntity
 {
-    Task<T?> GetByIdAsync(int id, bool isForDelete);
+    Task<T?> GetByIdAsync(int id, bool isForDelete = false);
     Task<IEnumerable<T>> GetAllAsync();
     Task<T> AddAsync(T entity);
-    Task<T> RemoveAsync(T entity);
+    Task RemoveAsync(T entity);
 }

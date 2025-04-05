b205c1eb1d0aa18548d20e3f32adb779bb4e5f79
Pierre Bossis <pbossis@hotmail.com>
2025-04-05 15:14:30 +0200

    Ajout DataContext

---
+++ factopia-backend.DAL/Database/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace factopia_backend.DAL.Database;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+}
